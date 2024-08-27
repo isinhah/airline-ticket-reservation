e286052abdd0476d5babfcea28d7168ab33d7a51
Isabel Henrique <contato.isabelhenrique@gmail.com>
2024-08-27 05:35:20 -0300

    CORS

---
+++ src/main/java/com/airline_ticket/api/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.airline_ticket.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
